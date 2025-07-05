43bd00e01573d0017f70f2de8bff7aa42fa70574
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-07-05 20:49:35 +0100

    testing the implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AirtimeTransactions } from './entities/AirtimeTransactions';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async findAll(
+    @Query('userId') userId?: string,
+    @Query('transactionId') transactionId?: string,
+  ) {
+    const query = AirtimeTransactions.createQueryBuilder('txn');
+
+    if (userId) {
+      query.andWhere('txn.userId = :userId', { userId });
+    }
+
+    if (transactionId) {
+      query.andWhere('txn.transactionId = :transactionId', { transactionId });
+    }
+
+    const transactions = await query.orderBy('txn.createdAt', 'DESC').getMany();
+
+    return { data: transactions };
   }
 }
