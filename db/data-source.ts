9e2ccdeeb7f0f84fe7ca4e72079c92df73a824d1
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-07-05 19:35:35 +0100

    update

---
+++ db/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  entities: ['dist/**/*.entity.js'],
+  host:
+    process.env.DB_HOST ||
+    'd1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  port: parseInt(process.env.DB_PORT as any) || 3306,
+  username: process.env.DB_USER || 'oyd3i2rs2yb26lic',
+  password: process.env.DB_PASSWORD || 'mpnfrleo095gheeb',
+  database: process.env.DB_NAME || 'wz12oseoex68yd4p',
+  logging: false,
+  migrations: ['dist/db/migrations/*.js'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'd1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      port: 3306,
+      username: 'oyd3i2rs2yb26lic',
+      password: 'mpnfrleo095gheeb',
+      database: 'wz12oseoex68yd4p',
+      entities: ['dist/src/entities/entities/*.js'],
+      synchronize: true,
+      logging: ['error'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
