7c4ee44f21c310361ab3066da365b05f5d6e254f
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-07-06 18:02:39 +0100

    removing db fallbacks

--- db/data-source.ts
+++ db/data-source.ts
@@ -5,6 +5,8 @@ import * as path from 'path';
 
 const ENV = process.env.NODE_ENV ?? 'development';
 
+console.log(ENV);
+
 const envFile = path.resolve(process.cwd(), `.env.${ENV}`);
 if (fs.existsSync(envFile)) {
   dotenv.config({ path: envFile });
@@ -13,15 +15,19 @@ if (fs.existsSync(envFile)) {
 } else {
   dotenv.config(); // fallback to default .env
   console.log(`⚠️  ${envFile} not found. Loaded fallback .env`);
+
+  if (!process.env.DB_DATABASE) {
+    throw new Error('DB_DATABASE is not set!');
+  }
 }
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT as any) || 3307,
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'algomachine',
-  database: process.env.DB_DATABASE || 'blue_dev',
+  port: parseInt(process.env.DB_PORT as any),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['dist/src/entities/*.js'], // ✅ match AppModule
   migrations: ['dist/db/migrations/*.js'],
 };
